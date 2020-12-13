31fa4600bd770df1c1081b92bbd20ae9ef347347
skymiku39 <catchmoonshooter@gmail.com>
2020-12-13 23:25:37 +0800

    feat✨(js): 確認按鈕的功能_switch

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var areaChoose = null;
+
 //初始化函數
 function Init() {
   // 區域按鈕
@@ -19,28 +21,39 @@ function Init() {
 function RandomArea_Clicked() {
   var left = document.getElementById("left");
   left.textContent = "隨機";
-  // 跳出視窗
-  // alert("都市");
+  areaChoose = "隨機";
 }
 
 function MetropolitanArea_Clicked() {
   var left = document.getElementById("left");
   left.textContent = "都市";
-  // 跳出視窗
-  // alert("都市");
+  areaChoose = "都市";
 }
 
 function RuralArea_Clicked() {
   var left = document.getElementById("left");
   left.textContent = "鄉村";
-  // 跳出視窗
-  // alert("都市");
+  areaChoose = "鄉村";
 }
 
 function Confirm_Clicked() {
   document.getElementById("left").textContent = "";
-  // 跳出視窗
-  // alert("都市");
+  switch (areaChoose) {
+    case "隨機":
+      alert("隨機");
+      break;
+    case "都市":
+      alert("都市");
+      break;
+    case "鄉村":
+      alert("鄉村");
+      break;
+    case null:
+      alert("null");
+      break;
+    default:
+      break;
+  }
 }
 
 Init();
