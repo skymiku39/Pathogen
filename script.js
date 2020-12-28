0f6a509895189086c9d7281c7a44228c7f77773e
skymiku39 <catchmoonshooter@gmail.com>
2020-12-29 00:29:17 +0800

    feat✨(js): 新增隨機可視化函數

--- script.js
+++ script.js
@@ -42,11 +42,30 @@ function randomArea(Area) {
   switch (Area) {
     case "都市":
       var result = Math.floor(Math.random() * MetropolitanAreaSubtag.length);
+      randomVisualize(Area, result);
       return "都市－" + MetropolitanAreaSubtag[result];
 
     case "鄉村":
       var result = Math.floor(Math.random() * RuralAreaSubtag.length);
+      randomVisualize(Area, result);
       return "鄉村－" + RuralAreaSubtag[result];
+
+    default:
+      break;
+  }
+}
+
+//隨機可視化
+function randomVisualize(Area, Result) {
+  //隨機結果的資料顯示測試
+  switch (Area) {
+    case "都市":
+      alert(MetropolitanAreaSubtag[Result]);
+      break;
+
+    case "鄉村":
+      alert(RuralAreaSubtag[Result]);
+      break;
 
     default:
       break;
